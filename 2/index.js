34818f26902c35f578ed21e950cf0774d716df43
= <martin.rosok@gmail.com>
2023-12-02 20:03:17 +0100

    wip

--- 2/index.js
+++ 2/index.js
@@ -1,1 +1,21 @@
-// work to do
+import fs from "fs";
+
+const lines = fs.readFileSync("./example.txt", "utf-8").trim().split("\n");
+var ids = [];
+
+let obj = {
+  red: 0,
+  blue: 0,
+  green: 0,
+};
+const games = lines[0].split(": ");
+const id = games[0].replaceAll(/\D/g, "");
+let turns = games[1].split(";");
+turns.forEach((turn) => {
+  let colors = turn.trim().split(", ");
+  colors.forEach((color) => {
+    let balls = color.split(" ");
+    obj[balls[1]] += Number(balls[0]);
+  });
+});
+console.log(obj);
