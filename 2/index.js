4e8c6d349cfa2a64362170c4c7154b831a6bb3f4
= <martin.rosok@gmail.com>
2023-12-02 21:17:02 +0100

    day 2, part 1 done

--- 2/index.js
+++ 2/index.js
@@ -1,35 +1,29 @@
 import fs from "fs";
 
 const lines = fs.readFileSync("./input.txt", "utf-8").trim().split("\n");
-var ids = [];
+var ids = new Set();
+const limits = {
+  red: 12,
+  blue: 14,
+  green: 13,
+};
 
 lines.forEach((line) => {
-  let obj = {
-    red: 0,
-    blue: 0,
-    green: 0,
-    redLimit: 12,
-    greenLimit: 13,
-    blueLimit: 14,
-  };
   const games = line.split(": ");
   const id = games[0].replaceAll(/\D/g, "");
-  let turns = games[1].split(";");
-  turns.forEach((turn) => {
-    let colors = turn.trim().split(", ");
-    colors.forEach((color) => {
-      let balls = color.split(" ");
-      obj[balls[1]] += Number(balls[0]);
-    });
-  });
-  if (
-    obj.red <= obj.redLimit &&
-    obj.green <= obj.greenLimit &&
-    obj.blue <= obj.blueLimit
-  ) {
-    console.log(id);
-    ids.push(Number(id));
+  const turns = games[1].split(";");
+  let abort = false;
+  for (let i in turns) {
+    let colors = turns[i].trim().split(", ");
+    for (let i in colors) {
+      let balls = colors[i].split(" ");
+      if (balls[0] > limits[balls[1]]) {
+        abort = true;
+      }
+    }
+  }
+  if (!abort) {
+    ids.add(Number(id));
   }
 });
-
-console.log(ids.reduce((s, v) => s + v));
+console.log([...ids].reduce((s, v) => s + v));
