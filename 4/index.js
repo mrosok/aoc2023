562e9eb7eaf847fe2792749fefa1f7c8e843824d
Martin Røsok <martin.rosok@netnordic.com>
2023-12-04 15:15:09 +0100

    more clean up

--- 4/index.js
+++ 4/index.js
@@ -1,34 +1,26 @@
 import fs from "fs";
 
 function part1() {
-  const lines = fs
-    .readFileSync("./input.txt", "utf-8")
-    .trim().split("\n");
+  const lines = fs.readFileSync("./input.txt", "utf-8").trim().split("\n");
   let sumPoints = [];
   lines.forEach((line) => {
     line = line.split(": ");
     line.shift();
     line = line.toString();
     const numbers = line.split(" | ");
-    let winningNumbers = numbers[0].split(" ").filter(n => n).map(Number);
-    let userNumbers = numbers[1].split(" ").filter(n => n).map(Number);
+    let winningNumbers = numbers[0].split(" ").filter((n) => n).map(Number);
+    let userNumbers = numbers[1].split(" ").filter((n) => n).map(Number);
     let hits = 0;
     for (let i in userNumbers) {
       if (winningNumbers.includes(userNumbers[i])) {
         hits++;
       }
     }
-    sumPoints.push(calculatePoints(hits));
+    if (hits) {
+      sumPoints.push(2 ** (hits - 1));
+    }
   });
-  console.log(sumPoints.reduce((s, v) => s+ v));
+  console.log(sumPoints.reduce((s, v) => s + v));
 }
 
 part1();
-
-function calculatePoints(hits) {
-  let points = 0
-  for (let i=0; i<hits; i++) {
-    points = (i == 0) ? 1 : points * 2;
-  }
-    return points;
-}
