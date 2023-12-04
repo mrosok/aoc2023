1acaf4f6eb94e8fc9da36782f5141d0a5e0d4e86
= <martin.rosok@gmail.com>
2023-12-04 18:11:49 +0100

    day 4 part 2

--- 4/index.js
+++ 4/index.js
@@ -1,15 +1,19 @@
 import fs from "fs";
+const lines = fs.readFileSync("./input.txt", "utf-8").trim().split("\n");
 
 function part1() {
-  const lines = fs.readFileSync("./input.txt", "utf-8").trim().split("\n");
   let sumPoints = [];
   lines.forEach((line) => {
-    line = line.split(": ");
-    line.shift();
-    line = line.toString();
-    const numbers = line.split(" | ");
-    let winningNumbers = numbers[0].split(" ").filter((n) => n).map(Number);
-    let userNumbers = numbers[1].split(" ").filter((n) => n).map(Number);
+    let [id, numbers] = line.split(": ");
+    let [winningNumbers, userNumbers] = numbers.split(" | ");
+    winningNumbers = winningNumbers
+      .split(" ")
+      .filter((n) => n)
+      .map(Number);
+    userNumbers = userNumbers
+      .split(" ")
+      .filter((n) => n)
+      .map(Number);
     let hits = 0;
     for (let i in userNumbers) {
       if (winningNumbers.includes(userNumbers[i])) {
@@ -24,3 +28,36 @@ function part1() {
 }
 
 part1();
+
+function part2() {
+  let cardCount = new Array(lines.length).fill(1);
+
+  lines.forEach((line, index) => {
+    let [id, numbers] = line.split(": ");
+    let [winningNumbers, userNumbers] = numbers.split(" | ");
+    winningNumbers = winningNumbers
+      .split(" ")
+      .filter((n) => n)
+      .map(Number);
+    userNumbers = userNumbers
+      .split(" ")
+      .filter((n) => n)
+      .map(Number);
+    let hits = 0;
+    for (let i in userNumbers) {
+      if (winningNumbers.includes(userNumbers[i])) {
+        hits++;
+      }
+    }
+    if (hits) {
+      for (let i = index + 1; i < index + 1 + hits; i++) {
+        if (cardCount[i]) {
+          cardCount[i] += cardCount[index];
+        }
+      }
+    }
+  });
+  console.log(cardCount.reduce((s, v) => s + v));
+}
+
+part2();
