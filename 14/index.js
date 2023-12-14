399e7c993ae86fd7f3972b24c6d7e443ba267a1c
mrosok <martin.rosok@gmail.com>
2023-12-14 19:39:22 +0100

    day 14 part 2

--- 14/index.js
+++ 14/index.js
@@ -6,22 +6,35 @@ let grid = fs
   .split("\n")
   .map((n) => n.split(""));
 
-console.log(grid[0][0] == "O");
-for (let x in grid) {
-  for (let y in grid[x]) {
-    if (grid[x][y] == "O") {
-      moveNorth(x, y);
+function part1() {
+  move();
+  console.log(flatten(grid));
+  console.log(score());
+}
+
+function part2() {
+  let target = 10 ** 9;
+  let t = 0;
+  let map = new Map();
+  while (t < target) {
+    cycle();
+    const hash = flatten(grid);
+    if (map.has(hash)) {
+      const start = map.get(hash);
+      const length = t - start;
+      const remain = target - 1 - t;
+      const mod = remain % length;
+      for (let i = 0; i < mod; i++) {
+        cycle();
+      }
+      console.log(score());
+      break;
+    } else {
+      map.set(hash, t);
+      t++;
     }
   }
 }
-const gridReversed = grid.reverse();
-let sum = 0;
-for (let i in gridReversed) {
-  for (let ch of gridReversed[i]) {
-    if (ch == "O") sum += Number(i) + 1;
-  }
-}
-console.log(sum);
 
 function moveNorth(x, y) {
   if (x > 0) {
@@ -34,3 +47,33 @@ function moveNorth(x, y) {
     }
   }
 }
+function move() {
+  for (let x in grid) {
+    for (let y in grid[x]) {
+      if (grid[x][y] == "O") {
+        moveNorth(x, y);
+      }
+    }
+  }
+}
+function cycle() {
+  for (let i = 0; i < 4; i++) {
+    move();
+    grid = a2d.rrotate(grid);
+  }
+}
+function score() {
+  let sum = 0;
+  for (let i in grid) {
+    for (let ch of grid[i]) {
+      if (ch == "O") sum += grid.length - i;
+    }
+  }
+  return sum;
+}
+
+function flatten(grid) {
+  return grid.map((line) => line.join("")).join("\n");
+}
+
+part2();
